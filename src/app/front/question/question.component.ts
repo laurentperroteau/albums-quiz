bba93d93a9a68f24a02e8a065ddef58b164e5644
laurentperroteau <laurentperroteau@gmail.com>
2017-08-29 18:32:39 +0200

    Move question form in component

--- src/app/front/question/question.component.ts
+++ src/app/front/question/question.component.ts
@@ -1,23 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Question } from './question.model';
 import { QuestionService } from '../services/question.service';
 
 @Component({
   selector: 'app-front-question',
   template: `
-    <form [formGroup]="question.form">
-      <input formControlName="id"/>
-      <input formControlName="label"/>
-      <md-radio-group formControlName="radioResponse">
-        <div formArrayName="radios">
-          <md-radio-button *ngFor="let radio of radios.controls;" [value]="radio.value">
-            {{ radio.value.label }}
-          </md-radio-button>
-        </div>
-      </md-radio-group>
-      <button (click)="submit()">Submit</button>
-    </form>
+    <app-front-question-form
+      [form]="question.form"
+      (onSubmit)="submit()">
+    </app-front-question-form>
     <pre>{{ question.form.value | json }}</pre>
     <hr/>
     <app-firebase></app-firebase>
@@ -26,10 +17,7 @@ import { QuestionService } from '../services/question.service';
 export class QuestionComponent {
   question: Question;
 
-  get radios(): FormArray { return this.question.form.get('radios') as FormArray; }
-
   constructor(
-    private _fb: FormBuilder,
     private _questionService: QuestionService
   ) {
     this.question = this._questionService.getWithForm();
